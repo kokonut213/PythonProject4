46d98669ec8186f29c2f48d6b5305c0b0db1a13d
anh hoang <hoangvb11xs@gmail.com>
2025-02-06 16:50:04 +0700

    OOP practice

---
+++ homework2_3.py
@@ -0,0 +1,41 @@
+class Animal:
+    def __init__(self,name):
+        self.name=name
+    def make_sound(self):
+        return "animal sound"
+    def get_name(self):
+        return self.name
+
+class Dog(Animal):
+    def __init__(self,name,breed):
+     super().__init__(name)
+     self.breed=breed
+    def make_sound(self):
+        return"woof,woof"
+    def get_breed(self):
+        return self.breed
+
+class Cat(Animal):
+    def __init__(self,name,color):
+        super().__init__(name)
+        self.color=color
+
+    def make_sound(self):
+        return"meow"
+
+    def get_color(self):
+        return self.color
+
+
+
+
+animal = Animal("Generic animal")
+dog = Dog(name="Buddy",breed="Husky")
+cat = Cat(name="Kitty",color="White")
+
+animals = [animal,dog,cat]
+for pet in animals:
+     print(f"{pet.get_name()},says:{pet.make_sound()}")
+     print(f"{dog.get_name()} is a:{dog.get_breed()}")
+     print(f"{cat.get_name()} is {cat.get_color()}")
+
